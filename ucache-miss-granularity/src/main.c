7fc67564f0aabee10bd669d35642b8c386f6a522
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2020-05-10 07:57:22 +0300

    JMP in the middle of cache lines routines

--- ucache-miss-granularity/src/main.c
+++ ucache-miss-granularity/src/main.c
@@ -3,5 +3,5 @@
 #define ITERATION_COUNT 1L << 31
 
 int main(void){
-    ucmc_64b_nopax8_6(ITERATION_COUNT);
+    ucmc_64b_nopax7jmp_2(ITERATION_COUNT);
 }
--- ucache-miss-granularity/src/ucache-miss-check.h
+++ ucache-miss-granularity/src/ucache-miss-check.h
@@ -6,8 +6,9 @@
 //ucmc prefix stands for UCache_Miss_Check
 
 /**
- * The ucmc_64b_nopax8_n routine uns 8*n times repeated nop ax iteration_count times.
+ * The ucmc_64b_nopax8_n routine is 8*n times repeated nop ax iteration_count times.
  * nop ax is 4 bytes long so 8 nop ax's consumes the entire 32 byte region of uops-cache.
+ *
  * The function code is 64-byte aligned.
  */
 void ucmc_64b_nopax8_1(size_t iteration_count);
@@ -16,5 +17,21 @@ void ucmc_64b_nopax8_3(size_t iteration_count);
 void ucmc_64b_nopax8_4(size_t iteration_count);
 void ucmc_64b_nopax8_5(size_t iteration_count);
 void ucmc_64b_nopax8_6(size_t iteration_count);
+
+/**
+ * The ucmc_64b_nopax7jmp_n routines consist of 7 times repeated nop ax instructions following
+ * an unconditional jmp to the next 32-byte aligned chunk of code. The chunk of code in turns
+ * contains 8 times repeated nop ax then again 7 times nop ax following an unconditional jmp.
+ *
+ * n is the total number of such blocks (either times 7 nop ax - jmp or times 8 nop ax)
+ * 
+ * The function code is 64-byte aligned.
+ */
+void ucmc_64b_nopax7jmp_1(size_t iteration_count);
+void ucmc_64b_nopax7jmp_2(size_t iteration_count);
+void ucmc_64b_nopax7jmp_3(size_t iteration_count);
+void ucmc_64b_nopax7jmp_4(size_t iteration_count);
+void ucmc_64b_nopax7jmp_5(size_t iteration_count);
+void ucmc_64b_nopax7jmp_6(size_t iteration_count);
 
 #endif //UCACHE_MISS_CHECK_H
