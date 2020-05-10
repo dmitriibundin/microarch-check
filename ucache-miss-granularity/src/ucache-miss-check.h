2bf26f9458f2d73db59ac5ddc572285407ee19f0
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2020-05-11 01:06:42 +0300

    Documentation added

--- ucache-miss-granularity/src/ucache-miss-check.h
+++ ucache-miss-granularity/src/ucache-miss-check.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-//ucmc prefix stands for UCache_Miss_Check
+//The 'ucmc' prefix stands for UCache_Miss_Check
 
 /**
  * The ucmc_64b_nopax8_n routine is 8*n times repeated nop ax iteration_count times.
@@ -20,7 +20,7 @@ void ucmc_64b_nopax8_6(size_t iteration_count);
 
 /**
  * The ucmc_64b_nopax7jmp_n routines consist of 7 times repeated nop ax instructions following
- * an unconditional jmp to the next 32-byte aligned chunk of code. The chunk of code in turns
+ * an unconditional jmp to the beginning of the next 32-byte aligned chunk of code. The chunk of code in turns
  * contains 8 times repeated nop ax then again 7 times nop ax following an unconditional jmp.
  *
  * n is the total number of such blocks (either times 7 nop ax - jmp or times 8 nop ax)
@@ -35,7 +35,12 @@ void ucmc_64b_nopax7jmp_5(size_t iteration_count);
 void ucmc_64b_nopax7jmp_6(size_t iteration_count);
 
 /**
+ * The ucmc_64b_nopax8nop19jmp_n routines consist of 8 times repeated nop ax following 
+ * 19 of nops ending with an unconditional jmp to the beginning of the next 32-byte aligned chunk of code.
  *
+ * n is the total number of 'times 8 nop ax times 19 nop jmp' blocks
+ *
+ * The function code is 64-byte aligned.
  */
 void ucmc_64b_nopax8nop19jmp_1(size_t iteration_count);
 void ucmc_64b_nopax8nop19jmp_2(size_t iteration_count);
@@ -43,4 +48,5 @@ void ucmc_64b_nopax8nop19jmp_3(size_t iteration_count);
 void ucmc_64b_nopax8nop19jmp_4(size_t iteration_count);
 void ucmc_64b_nopax8nop19jmp_5(size_t iteration_count);
 void ucmc_64b_nopax8nop19jmp_6(size_t iteration_count);
+
 #endif //UCACHE_MISS_CHECK_H
